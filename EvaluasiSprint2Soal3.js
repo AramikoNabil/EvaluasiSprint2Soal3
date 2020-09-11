82a726ff94be59bd04569e9e8de2eea75338893e
sleepy81k <nabilnico@gmail.com>
2020-09-12 06:48:57 +0700

    first

---
+++ EvaluasiSprint2Soal3.js
@@ -0,0 +1,7 @@
+const hitungLuasPersegiPanjang = (a, b) => {
+    return (a * b)
+    
+}
+    document.write ("Panjang : 5 <br>" )
+    document.write ("Lebar : 4 <br>")
+    document.write ("Luas Persegi Panjang :" + hitungLuasPersegiPanjang  (5 , 4 ) )
